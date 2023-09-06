4a5a6aada631c36a738c6b01df9c07753491c69b
AISWARIA-A <aiswariaanilkumar@gmail.com>
2023-09-07 00:04:10 +0530

    Day 18

---
+++ Day18/LibraryImageApp/LibraryImageApp/Controllers/BookController.cs
@@ -0,0 +1,96 @@
+﻿using LibraryImageApp.Models;
+using LibraryImageApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryImageApp.Controllers
+{
+    public class BookController : Controller
+    {
+        BookRepository bookRepository = new BookRepository();
+        // GET: Book
+        public ActionResult Index()
+        {
+            ModelState.Clear();
+            return View(bookRepository.GetAllBooks());
+        }
+
+        public ActionResult AddDetails()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddDetails(Book book)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    HttpPostedFileBase image = Request.Files["CoverImage"];
+
+                    if (image != null && image.ContentLength > 0)
+                    {
+                        using (var binaryReader = new BinaryReader(image.InputStream))
+                        {
+                            book.Cover = binaryReader.ReadBytes(image.ContentLength);
+                        }
+
+                        if (bookRepository.AddBooks(book, image))
+                        {
+                            ViewBag.Message = "Book details added successfully";
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("Cover", "Please select a valid cover image.");
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        public ActionResult EditDetails(int? id)
+        {
+            return View(bookRepository.GetAllBooks().Find(book => book.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult EditDetails(int id, Book book)
+        {
+            try
+            {
+                bookRepository.EditDetails(book);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        public ActionResult DeleteDetails(int id)
+        {
+            if (bookRepository.DeleteDetails(id))
+            {
+                TempData["SuccessMessage"] = "Book details deleted successfully";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Unable to delete details";
+            }
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ Day18/LibraryImageApp/LibraryImageApp/Repository/BookRepository.cs
@@ -0,0 +1,137 @@
+﻿using LibraryImageApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace LibraryImageApp.Repository
+{
+    public class BookRepository
+    {
+        private SqlConnection connection;
+
+        private void Connection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["databaseConnection"].ToString();
+            connection = new SqlConnection(connectionString);
+        }
+
+        //To insert book details
+        public bool AddBooks(Book book, HttpPostedFileBase image)
+        {
+            Connection();
+            SqlCommand command = new SqlCommand("SPI_InsertABook", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@Title", book.Title);
+            command.Parameters.AddWithValue("@Author", book.Author);
+            command.Parameters.AddWithValue("@Category", book.Category);
+            byte[] Photobyte = ConvertToBytes(image);
+            command.Parameters.AddWithValue("@Cover", Photobyte);
+
+            connection.Open();
+            int i = command.ExecuteNonQuery();
+            connection.Close();
+            if (i >= 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+        //To get all book details
+
+        public List<Book> GetAllBooks()
+        {
+            Connection();
+            List<Book> bookList = new List<Book>();
+            SqlCommand command = new SqlCommand("SPS_GetBooks", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            connection.Open();
+            adapter.Fill(dataTable);
+            connection.Close();
+
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                // Assuming you have a column named "Cover" in your database to store image bytes.
+                byte[] coverBytes = (byte[])dataRow["Cover"];
+
+                bookList.Add(new Book
+                {
+                    Title = Convert.ToString(dataRow["Title"]),
+                    Author = Convert.ToString(dataRow["Author"]),
+                    Category = Convert.ToString(dataRow["Category"]),
+                    // Set the Cover property in your Book model with image bytes.
+                    Cover = coverBytes
+                });
+            }
+
+            return bookList;
+        }
+
+
+        ///To edit book details
+
+        public bool EditDetails(Book book)
+        {
+            Connection();
+            SqlCommand command = new SqlCommand("SPU_UpdateBook", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@Title", book.Title);
+            command.Parameters.AddWithValue("@Author", book.Author);
+            command.Parameters.AddWithValue("@Category", book.Category);
+            connection.Open();
+            int i = command.ExecuteNonQuery();
+            connection.Close();
+            if (i >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private byte[] ConvertToBytes(HttpPostedFileBase image)
+        {
+            if (image != null)
+            {
+                byte[] imageBytes = new byte[image.ContentLength];
+                image.InputStream.Read(imageBytes, 0, image.ContentLength);
+                return imageBytes;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// To delete book details by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+
+        public bool DeleteDetails(int id)
+        {
+            Connection();
+            SqlCommand command = new SqlCommand("SPD_DeleteBook", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@Id", id);
+
+            connection.Open();
+            int i = command.ExecuteNonQuery();
+            connection.Close();
+            if (i >= 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+    }
+}
